b20c0e3f423f1272d0b2043c6ab881071fa2ae8c
anyars-encarta <anyarsencarta@gmail.com>
2024-03-03 12:24:47 +0000

    Correct eslint errors and install proptypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from './components/weather';
 
--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,18 +1,15 @@
-const Search = ({ search, setSearch, handleSearch }) => {
+const Search = ({ search, setSearch, handleSearch }) => (
+  <div className="search-engine">
+    <input
+      type="text"
+      placeholder="Search for a city"
+      name="search"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+    />
 
-    return (
-        <div className="search-engine">
-            <input
-                type="text"
-                placeholder="Search for a city"
-                name="search"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-            />
-
-            <button onClick={handleSearch}>Search Weather</button>
-        </div>
-    )
-};
+    <button onClick={handleSearch}>Search Weather</button>
+  </div>
+);
 
 export default Search;
